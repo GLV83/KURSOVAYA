f5ac29ee606666b53763b7c9f1015987d509e58e
GLV83 <35852609+GLV83@users.noreply.github.com>
2019-02-06 03:48:22 +0300

    Create constanty.py

---
+++ constanty.py
@@ -0,0 +1,9 @@
+USER_ROLES = ['CEO', 'CFO', 'CSM']
+
+GRAPTH_TYPES = {'sum_revenue': 'Сумма выручки',
+                'sum_deb_receivables': 'Сумма дебиторской задолженности',
+                'sum_cred_receivables': 'Сумма кредитной задолженности',
+                'sum_arrived': 'Сумма прибыли',
+                'sum_pre_calc': 'Сумма предварительных расчетов',
+                'sum_surcharges': 'Сумма доплат',
+                'sum_prepay': 'Сумма предоплат'}
